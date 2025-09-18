9f1f09d837c025c543a50b08f0d97036a5f67e82
Gummadi Charan <gummadicharan37@gmail.com>
2025-09-18 11:12:38 +0530

    ABS, SRM and ULT Very Mini Project Final Push

--- ABS.sh
+++ ABS.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 echo "Automatic Backup Script"
+echo "---------------------------"
+
+# Variables
+Backup_DIR="/mnt/c/Users/DELL/Desktop/Shell_Scripting_Basic_Projects/backups"
+TimeStamp=$(date +"%Y-%m-%d_%H-%M-%S")
+BackUpFile="backup_$TimeStamp.tar.gz"
+LogFile="$Backup_DIR/backup.log"
+
+# Make sure backup directory exists
+mkdir -p "$Backup_DIR"
+
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] - Starting Backup..."
+
+# Create tar.gz archive of your files
+tar -cvzf "$Backup_DIR/$BackUpFile" backupFile1.sh backupFile.py ABS.sh
+
+# Log the backup
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Backup created: $BackUpFile" >> "$LogFile"
+
+echo "Backup completed. File saved to $Backup_DIR/$BackUpFile"
+
